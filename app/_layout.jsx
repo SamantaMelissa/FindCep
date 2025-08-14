e82b0c6e8f2b36b65fb00fd5ae08e67032411d0d
Samanta Melissa <samanta.melissa13@gmail.com>
2025-08-14 17:38:54 -0300

    feat: Adicionando a fonte poppins

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,26 @@
-import { Stack } from "expo-router";
 
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font"
+import { ActivityIndicator, View } from "react-native";
 export default function RootLayout() {
-  // return <Stack />;
-  {/* Desativando aquele meu padrão */}
-  //Primeira opção:
+ 
+  const [fontsLoaded] = useFonts ({
+    "Poppins-Regular": require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+
+    "Poppins-Bold": require("../assets/fonts/Poppins/Poppins-Bold.ttf")
+  })
+
+  // Enquanto nao carregar as fonts mostrar loading!!
+
+  if (!fontsLoaded) {
+    return(
+      <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    )
+  }
+
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: '', headerTransparent: true, headerShown: false }} />
--- app/index.jsx
+++ app/index.jsx
@@ -2,27 +2,68 @@ import { Text, View, StyleSheet, ImageBackground, Image, ScrollView } from "reac
 import { Input } from '../components/input/input';
 import { Botao } from '../components/botao/botao';
 import { Card } from '../components/card/card';
+import { useState } from "react";
+
+import axios from 'axios';
+
 export default function Index() {
+
+  const [cep, setCep] = useState("");
+  const [jsonCep, setJsonCep] = useState({});
+
+  const [aparecidinho, setAparecidinho ] = useState(false);
+
+  async function consultarCep(e) {
+    e.preventDefault();
+    try {
+      if (cep !== "" && cep.length === 8) {
+        const resposta = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        setJsonCep(resposta.data);
+        // console.log(jsonCep.logradouro);
+        setAparecidinho(true);
+      } else {
+        alert("O cep está incorreto. Digite com 8 números!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       {/* 1. Logo + imagem de fundo */}
       <ScrollView style={styles.containerScroll}>
-      <View style={styles.containerImg}>
-        <ImageBackground source={require('../assets/images/ImgFundo.png')}
-          style={styles.imgFundo}>
-          <Image source={require('../assets/images/LogoFindCEP.png')} style={styles.logo}></Image>
-        </ImageBackground>
-      </View>
-      {/* 2. Campo de consulta */}
+        <View style={styles.containerImg}>
+          <ImageBackground source={require('../assets/images/ImgFundo.png')}
+            style={styles.imgFundo}>
+            <Image source={require('../assets/images/LogoFindCEP.png')} style={styles.logo}></Image>
+          </ImageBackground>
+        </View>
+        {/* 2. Campo de consulta */}
         <View style={styles.container}>
           {/* 2.1. Título */}
           <Text style={styles.titulo}>Consulte seu CEP</Text>
           {/* 2.2. Input */}
-          <Input />
+          <Input
+            valorCep={cep}
+            onChangeValorCep={e => { setCep(e); console.log(e); }}
+          />
           {/* 2.3. Botão */}
-          <Botao tituloBotao='Consultar' />
+          <Botao
+            tituloBotao='Consultar'
+            onPress={consultarCep}
+          />
           {/* 2.3. Card de informações */}
-          <Card />
+          {aparecidinho &&
+            <Card
+              cep={jsonCep.cep}
+              logradouro={jsonCep.logradouro}
+              bairro={jsonCep.bairro}
+              uf={jsonCep.uf}
+              estado={jsonCep.estado}
+              regiao={jsonCep.regiao}
+            />
+          }
         </View>
       </ScrollView>
 
@@ -32,8 +73,8 @@ export default function Index() {
 
 //Estilos dos meus componentes:
 const styles = StyleSheet.create({
-  containerImg:{
-    height:'30%'
+  containerImg: {
+    height: '30%'
 
   },
   imgFundo: {
@@ -60,6 +101,8 @@ const styles = StyleSheet.create({
     paddingBottom: 80,
   },
   titulo: {
-    fontSize: 25
+    fontSize: 25,
+    fontFamily: "Poppins-Bold",
+    color: '#000000'
   }
 })
